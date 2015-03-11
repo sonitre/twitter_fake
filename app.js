a1070ecb87ce1d243affab3aa9624a14acd716c1
Sonia Trehan <soniatrehan6@gmail.com>
2015-03-11 15:59:34 -0400

    made scaffolding

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require( 'express' );
+var morgan = require( 'morgan' )
+var app = express();
+
+app.use(morgan('dev'));
+
+app.listen(3000);
+
+app.get('/', function(req, res){
+	res.send('Hello World');
+});
+
+app.get('/news', function(req, res){
+	res.send('Bye bye john');
+});
