ca7f037f948a9814219f3d6b1f4d5566a9fbaf38
Sonia Trehan <soniatrehan6@gmail.com>
2015-03-11 17:03:49 -0400

    Front page route working

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ var express = require( 'express' );
 var morgan = require( 'morgan' );
 var swig = require('swig');
 var app = express();
+var routes = require('./routes/');
 
 
+app.use('/', routes);
+
+//Listen on port 3000, turn off caching
 app.use(morgan('dev'));
 
 app.listen(3000);
 
-app.get('/', function(req, res){
-	res.send('Hello World');
-});
+swig.setDefaults({ cache: false });
 
-app.get('/news', function(req, res){
-	res.send('Bye bye john');
-});
+app.use(express.static(__dirname + '/public'));
+
+//Declare data
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+//Render templates with swig
+app.engine("html", swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+
